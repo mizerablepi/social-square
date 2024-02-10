bea2a5eedbb2385b0463d0a8df7cc5d69925bad7
maaz <mizerablepi@gmail.com>
2024-02-11 01:37:55 +0530

    reorganise

--- src/components/Body.jsx
+++
@@ -1,5 +0,0 @@
-function Body() {
-  return <div className="flex-grow-[2]">BODY </div>;
-}
-
-export default Body;
--- src/components/Extra.jsx
+++
@@ -1,5 +0,0 @@
-function Extra() {
-  return <div className="flex-1 hidden md:block ">EXTRA</div>;
-}
-
-export default Extra;
