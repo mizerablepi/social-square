241e111acbd23183a1fad8227ed239c607d93e99
maaz <mizerablepi@gmail.com>
2024-02-10 20:42:20 +0530

    add default profile pic

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import Post from "@/models/Post";
 
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
@@ -16,7 +15,11 @@ const userSchema = new Schema({
     unique: true,
   },
   password: { type: String, required: true },
-  profilePicture: { type: String },
+  profilePicture: {
+    type: String,
+    default:
+      "https://res.cloudinary.com/db5sjzgsb/image/upload/v1707502544/profiles/odtcmz1q334bzepirev6.png",
+  },
   followers: [{ type: Schema.Types.ObjectId, ref: this }],
   following: [{ type: Schema.Types.ObjectId, ref: this }],
   posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
