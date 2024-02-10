7ad054dfc76a04792549a84a479839b08f239ce8
maaz <mizerablepi@gmail.com>
2024-02-11 01:37:43 +0530

    add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <>Index</>;
+import getUser from "@/helper/getUser";
+import Aside from "@/components/home/Aside";
+import Body from "@/components/home/Body";
+import Extra from "@/components/home/Extra";
+import MobileHeader from "@/components/MobileHeader";
+import MobileNavbar from "@/components/MobileNavbar";
+
+export default async function Home() {
+  const user = await getUser();
+
+  return (
+    <div className="flex gap-6">
+      <Aside profilePic={user.profilePicture} />
+      <div className="flex-grow-[2] flex flex-col min-h-screen">
+        <MobileHeader profilePic={user.profilePicture} />
+        <Body />
+        <MobileNavbar />
+      </div>
+      <Extra />
+    </div>
+  );
 }
