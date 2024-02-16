53219a49225730f5a23e145e698d1e608612fb70
miz <mizerablepi@gmail.com>
2024-02-16 21:13:39 +0530

    clean page jsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,14 @@ import getUser from "@/helper/getUser";
 import Aside from "@/components/home/Aside";
 import Body from "@/components/home/Body";
 import Extra from "@/components/home/Extra";
-import MobileHeader from "@/components/MobileHeader";
-import MobileNavbar from "@/components/MobileNavbar";
 
 export default async function Home() {
   const user = await getUser();
 
   return (
-    <div className="flex gap-6 max-w-[90rem] mx-auto">
-      <Aside profilePic={user.profilePicture} />
-      <div className="flex-[2] flex flex-col min-h-screen">
-        <Body />
-      </div>
+    <div className="flex gap-2 max-w-[90rem] mx-auto">
+      <Aside user={user} />
+      <Body />
       <Extra />
     </div>
   );
