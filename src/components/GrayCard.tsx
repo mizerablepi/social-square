bf9f35b494d9c02dc6ede584eb2eb4c7d548b38e
miz <mizerablepi@gmail.com>
2024-02-16 21:13:51 +0530

    add clasname to graycard

--- src/components/GrayCard.tsx
+++ src/components/GrayCard.tsx
@@ -1,9 +1,13 @@
 function GrayCard({
   children,
+  classname = "",
 }: Readonly<{
   children: React.ReactNode;
+  classname?: string;
 }>) {
-  return <div className="rounded-lg bg-gray-800">{children}</div>;
+  return (
+    <div className={"rounded-lg bg-gray-800 " + classname}>{children}</div>
+  );
 }
 
 export default GrayCard;
