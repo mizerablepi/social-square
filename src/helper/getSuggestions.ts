94f2380596280f225291a686ec76537ea75cc0dc
miz <mizerablepi@gmail.com>
2024-02-17 12:26:40 +0530

    add function to get suggested posts

--- src/helper/getSuggestions.ts
+++ src/helper/getSuggestions.ts
@@ -2,20 +2,37 @@
 
 import User from "@/models/User";
 import { connect } from "@/dbConfig";
+import Post from "@/models/Post";
 
-export default async function getSuggestions(userId: string) {
+export async function getUserSuggestions(userId: string) {
   connect();
   const countOfUsers = await User.estimatedDocumentCount();
-  const numberOfUsersToSend = 3;
-  if (countOfUsers < numberOfUsersToSend) {
+  const NUMBER_OF_USER_TO_SEND = 3;
+  if (countOfUsers < NUMBER_OF_USER_TO_SEND) {
     const users = await User.find({ _id: { $ne: userId } }).exec();
     return users;
   } else {
-    const randomNumber = Math.random() * (countOfUsers - numberOfUsersToSend);
+    const randomNumber =
+      Math.random() * (countOfUsers - NUMBER_OF_USER_TO_SEND);
     const users = await User.find({})
       .skip(randomNumber)
-      .limit(numberOfUsersToSend)
+      .limit(NUMBER_OF_USER_TO_SEND)
       .exec();
     return users;
+  }
+}
+
+export async function getPostSuggestion(userId: string) {
+  connect();
+  const countOfPosts = await Post.estimatedDocumentCount();
+  const NUMBER_OF_POST_TO_SEND = 5;
+  if (countOfPosts < NUMBER_OF_POST_TO_SEND) {
+    const posts = await Post.find({ author: { $ne: userId } }).exec();
+    return posts;
+  } else {
+    const posts = await Post.find({ author: { $ne: userId } })
+      .sort({ publishedAt: -1, likes: -1 })
+      .limit(5);
+    return posts;
   }
 }
