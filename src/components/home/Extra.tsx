450e3d20619c4fafbc75fa70f4e33da48d48901a
miz <mizerablepi@gmail.com>
2024-02-19 12:05:03 +0530

    scaffold extra

--- src/components/home/Extra.tsx
+++ src/components/home/Extra.tsx
@@ -1,5 +1,15 @@
-function Extra() {
-  return <div className="flex-1 hidden md:block ">EXTRA</div>;
+import GrayCard from "../GrayCard";
+import { getPostSuggestion } from "@/helper/getSuggestions";
+
+async function Extra({ userId }: { userId: string }) {
+  const suggestedPosts = await getPostSuggestion(userId);
+  return (
+    <div className="flex-1 hidden lg:block pt-20">
+      <GrayCard classname="p-2">
+        <h4 className="font-bold mb-2">Trending Posts:</h4>
+      </GrayCard>
+    </div>
+  );
 }
 
 export default Extra;
