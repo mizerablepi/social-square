f20012765c40a80ea00d1d12c121230c8af172f8
miz <mizerablepi@gmail.com>
2024-02-19 12:04:18 +0530

    remove middleware

--- src/deprecatedmiddleware.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-export async function middleware(req: NextRequest) {}
-export const config = {
-  matcher: [`/((?!api|_next/static|_next/image|favicon.ico).*)`],
-};
