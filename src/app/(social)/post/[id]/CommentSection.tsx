64d639d747a011d7264eee7c2a26b1110ef4f19a
miz <mizerablepi@gmail.com>
2024-02-28 00:14:44 +0530

    add pagination

--- src/app/(social)/post/[id]/CommentSection.tsx
+++ src/app/(social)/post/[id]/CommentSection.tsx
@@ -1,16 +1,26 @@
 import GrayCard from "@/components/GrayCard";
 import CommentCard from "./CommentCard";
+import { useEffect, useState } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+import PageBar from "./Pagebar";
+import { useRouter } from "next/navigation";
+import { Oval } from "react-loader-spinner";
 
-function CommentSection({
-  post,
-}: {
-  post: {
-    comments: [
-      { text: string; author: { name: string; username: string }; _id: string }
-    ];
-    author: { username: string; name: string; profilePic: string };
-  };
-}) {
+function CommentSection({ postId }: { postId: string }) {
+  const query = useSearchParams();
+  const router = useRouter();
+  const path = usePathname();
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    fetch(
+      `/api/post/${postId}/comments?page=${query.get("page") ?? 1}&sort=${
+        query.get("sort") ?? -1
+      }`
+    )
+      .then((res) => res.json())
+      .then((res) => setComments(res.comments));
+  }, [postId, query]);
+
   return (
     <>
       <div className="flex justify-between">
@@ -19,16 +29,37 @@ function CommentSection({
           name="comment_sort"
           id="comment_sort"
           className="mr-4 bg-gray-700 rounded p-1 font-bold"
+          onChange={(e) =>
+            router.replace(
+              path +
+                `?page=${query.get("page")}&sort=${e.target.value}#comments`
+            )
+          }
+          value={query.get("sort") ?? "1"}
         >
-          <option value="newest">Newest</option>
-          <option value="oldest">Oldest</option>
+          <option value="1">Oldest</option>
+          <option value="-1">Newest</option>
         </select>
       </div>
       <hr className="mt-4 mb-2 border-slate-600" />
-
-      {post.comments.map((comment: any) => {
-        return <CommentCard comment={comment} key={comment._id} />;
-      })}
+      {comments.length > 0 ? (
+        <>
+          {comments.map((comment: any) => {
+            return <CommentCard comment={comment} key={comment._id} />;
+          })}
+        </>
+      ) : (
+        <div className="flex justify-center my-2">
+          <Oval
+            height="80"
+            width="80"
+            color="#53bfc5"
+            secondaryColor="53bfc5"
+            ariaLabel="oval-loading"
+          />
+        </div>
+      )}
+      <PageBar postId={postId} />
     </>
   );
 }
