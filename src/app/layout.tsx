41bb367c98a0dc798809ef226bb29f6a17c0ef33
miz <mizerablepi@gmail.com>
2024-02-28 00:14:11 +0530

    add fonts for mui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,10 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import MobileHeader from "@/components/MobileHeader";
 import MobileNavbar from "@/components/MobileNavbar";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
